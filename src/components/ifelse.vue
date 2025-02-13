85945c20f27616454ad93eaa7e9cca07645cd3f1
imran akram <imran.ackrm@gmail.com>
2025-02-13 18:04:38 +0500

    Create ifelse.vue
    if else in vue

---
+++ src/components/ifelse.vue
@@ -0,0 +1,21 @@
+<template>
+    <hr>
+    <h1>if else</h1>
+    <h2>Compoent Name :: {{ componentName }}</h2>
+    <h3>Condition is returning :: <span v-if="show">True</span><span v-else>False</span></h3>
+    <hr>
+</template>
+<script>
+    export default{
+        name: 'IfElseComponent',
+        data(){
+            return{
+                componentName: 'IfElseComponent',
+                show:false
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
