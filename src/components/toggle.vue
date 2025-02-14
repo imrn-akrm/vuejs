aaf0412278be0ef7c1aef087b55808996a0d066f
imran akram <imran.ackrm@gmail.com>
2025-02-14 17:51:34 +0500

    Create toggle.vue
    toggle in vue

---
+++ src/components/toggle.vue
@@ -0,0 +1,21 @@
+<template>
+    <hr>
+    <h1>toggle</h1>
+    <h2>component :: {{ componentName }}</h2>
+    <h2 v-if="show">True</h2>
+    <h2 v-else>False</h2>
+    <button v-on:click="show=!show">Toggle</button>
+
+    <hr>
+</template>
+<script>
+    export default{
+        name:'toggleComponent',
+        data(){
+            return{
+                compoentName:'Toggle',
+                show:true
+            }
+        }
+    }
+</script>
