11a44b04bbc6dccdaa30f35d5dfc2772a2d4876c
imran akram <imran.ackrm@gmail.com>
2025-02-15 23:47:26 +0500

    Create twoWayBinding.vue
    binding

---
+++ src/components/twoWayBinding.vue
@@ -0,0 +1,30 @@
+<template>
+    <hr>
+    <h1>Two Way Binding</h1>
+    <h2>{{ componentName  }}</h2>
+    <input type="text" v-model="count">
+    <h1>{{ count }}</h1>
+    <hr>
+</template>
+<script>
+    export default{
+        name: 'TwoWayBinding',
+        data(){
+            return{
+                componentName: "Component Name: Two Way Binding",
+                count:555
+            }
+        }
+    }
+</script>
+<style scoped>
+    h1{
+        color:brown;
+        font-size: small;
+        font-weight:bold;
+    }
+    h2{
+        color:maroon;
+        font-size: smaller;
+    }
+</style>
